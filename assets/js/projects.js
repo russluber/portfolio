3851751bae2901cbea8036b06887143fa728733f
russluber <russelhluber@gmail.com>
2025-06-30 22:49:39 -0700

    Move projects.js to appropriate folder and fixed source links

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,4 +1,4 @@
-import { fetchJSON, renderProjects } from '../assets/js/global.js';
+import { fetchJSON, renderProjects } from '../js/global.js';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 let query = '';
