872944c1d7cc002e30a90c6e2e87fc9f1a8ad934
russluber <russelhluber@gmail.com>
2025-06-30 22:59:48 -0700

    Add back to top button to all major pages of site

--- assets/js/global.js
+++ assets/js/global.js
@@ -104,6 +104,16 @@ toggle.addEventListener('click', () => {
     }
   });
 
+/*
+====================
+Back to Top Behavior
+====================
+*/
+
+document.querySelector('.back-to-top')?.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
 
 
   /*
